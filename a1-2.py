2227550d9d584ab35737bc57547ebfd1c2b81a7a
zeynsr <91886337+zeynsr@users.noreply.github.com>
2021-10-04 16:44:32 +0330

    Add files via upload

---
+++ a1-2.py
@@ -0,0 +1,8 @@
+r = int(input())
+pi = (3.14)
+
+P = 2*pi*r
+A = pi*r*r
+
+print(P)
+print(A)
---
+++ a1-4.py
@@ -0,0 +1,6 @@
+w=int(input('enter your weight(kg) : '))
+h=float(input('enter your height(m) : '))
+
+mbti=w/(h*h)
+
+print(mbti)
