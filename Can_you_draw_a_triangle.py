0c31a400a556f4842b3efcfbc558a54160d8b570
Zeynab Sarafraz <91886337+zeynsr@users.noreply.github.com>
2023-06-08 12:40:50 +0330

    rename
    Signed-off-by: Zeynab Sarafraz <91886337+zeynsr@users.noreply.github.com>

---
+++ Can_you_draw_a_triangle.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if( a < b + c ) & ( b < a + c ) & ( c < b + a ):
+    print('possible.')
+else :
+    print('impossible !')    
--- a1-3.py
+++
@@ -1,8 +0,0 @@
-a=int(input())
-b=int(input())
-c=int(input())
-
-if(a<b+c)&(b<a+c)&(c<b+a):
-    print('possible.')
-else :
-    print('impossible !')    
